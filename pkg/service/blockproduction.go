d5fcbf46dca8ac28b011327ab08dc6c3061c530d
shivesh <shivesh.safal@gather.network>
2023-03-10 14:16:50 +0530

    removed warning

--- pkg/service/blockproduction.go
+++ pkg/service/blockproduction.go
@@ -35,7 +35,7 @@ func (s *Service) checkBlockProduction() error {
 	} else {
 		if time.Now().Unix()-int64(s.lastBlockMinedAt) > int64(config.BlockProductionTime) {
 			emailMessage := "Alert ! \n Blocks time exceeds " + strconv.Itoa(config.BlockProductionTime) + " secounds ! \n\n" +
-				"Last block was " + string(s.lastBlock) + "\n" +
+				"Last block was " + strconv.Itoa(s.lastBlock) + "\n" +
 				"Last block was mined at : " + time.Unix(int64(s.lastBlockMinedAt), 0).String() + "\n" +
 				"\n\nImportant : Number of block time exceeding emails skipped because of frequent emails is " + strconv.Itoa(s.blockProductionEmails.countOfEmailsSkipped)
 			s.log.Infof(emailMessage)
